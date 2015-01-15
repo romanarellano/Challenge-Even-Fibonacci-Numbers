f9f8b366d811e3328e27abdcf2cd73075a36093a
romanarellano <romanarellano@yahoo.com>
2015-01-15 11:31:42 -1000

    fibonacci with 2 for loops

--- challenge_2.js
+++ challenge_2.js
@@ -1,6 +1,30 @@
-exports.sumOfEvenFibonacciNumbers = function(n){
+exports.
+sumOfEvenFibonacciNumbers = function(n){
   var sum = 0;
-  // do your work here
+var prev=0;
+var curr=1;
+var add=0;
+var array=[];
+  var result=0;
+     for(;add<n;){
+         
+         add= prev+curr;
+         array.push(add);
+         prev=curr;
+
+        curr=add;
+       
+
+     }
+
+for(var j=0;j<array.length;j++){
+
+   if( array[j]%2===0){
+        
+        result+=array[j];
+
+   }
+}
   
-  return sum;
+  return result;
 };
