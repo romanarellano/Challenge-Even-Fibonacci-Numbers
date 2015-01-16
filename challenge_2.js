bbf8994b592f312bcc0632f56bc5173623f6d03b
romanarellano <romanarellano@yahoo.com>
2015-01-15 15:16:04 -1000

    fibonacci using map,filter,reduce function

--- challenge_2.js
+++ challenge_2.js
@@ -1,30 +1,46 @@
-exports.
-sumOfEvenFibonacciNumbers = function(n){
+exports.sumOfEvenFibonacciNumbers = function(n){
   var sum = 0;
 var prev=0;
 var curr=1;
-//var add=0;
 var array=[];
   var result=0;
      while(sum<n){
          
          sum= prev+curr;
-         array.push(sum);
-         prev=curr;
+         array.push(sum);//array=[1,2,3,5,8]
+         prev=curr;//prev=3
 
-         curr=sum;
+         curr=sum;//curr=5
        
 
      }
 
-for(var j=0;j<array.length;j++){
+     var add= function(x,y){
+        return x+y;
 
-   if( array[j]%2===0){
-        
-        result+=array[j];
-
-   }
-}
+     };
+    var total= array.map(function(x){
   
-  return result;
+     if(x%2===0){
+        return x;
+       
+     }
+
+  }).filter(function(x){
+         return typeof(x) === "number";
+         
+       }).reduce(function(x,y){
+        return x+y;
+
+     },0);
+// for(var j=0;j<array.length;j++){
+
+//    if( array[j]%2===0){
+        
+//         result+=array[j];
+
+//    }
+// }
+  
+  return total;//result;
 };
